91efa441d60a1bfa48193237ec05b058aa0ca4e2
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-31 17:38:44 -0300

    Requisito 03

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,17 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function getEmployeeByName(employeeName) {
   // seu código aqui
+  if (employeeName === undefined) {
+    return { };
+  }
+
+  const colaborador = employees.find((pessoa) => pessoa
+    .firstName === employeeName || pessoa.lastName === employeeName);
+  return colaborador;
 }
+getEmployeeByName();
 
 module.exports = getEmployeeByName;

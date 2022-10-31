2b82e5316978300fb2a6b7d9cc88cf322c0079e6
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-31 14:47:23 -0300

    Requisito 01

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
+// console.log(data);
+
+const { species } = data;
+
+function getSpeciesByIds(...ids) {
   // seu código aqui
-  
+  const id = species.filter((specie) => ids.includes(specie.id));
+  return id;
 }
+getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce');
 
 module.exports = getSpeciesByIds;

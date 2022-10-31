3071111c4a798ed22006b35885d62df74219c9fd
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-10-31 16:48:42 -0300

    Requisito 02

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
+const { species } = data;
+// const { residents } = species;
+
 function getAnimalsOlderThan(animal, age) {
   // seu código aqui
+  const tipoEspecie = species.find((specie) => specie.name === animal);
+  return tipoEspecie.residents.every((resident) => resident.age >= age);
 }
+console.log(getAnimalsOlderThan('tigers', 50));
 
 module.exports = getAnimalsOlderThan;

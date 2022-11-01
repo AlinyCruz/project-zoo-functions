0a129254c7849e0cf8f41f45c26d2f8e7a8ef86c
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-01 19:35:51 -0300

    Requisito 05 parte

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,23 @@
-const data = require('../data/zoo_data');
+const data = require('../data/zoo_data'); // 1 objeto com 2 arrays (species e employees) e 2 objetos (hours e prices)
+
+const { species } = data; // 1 array com 9 objetos
+
+const { residents } = species;
 
 function countAnimals(animal) {
   // seu código aqui
+  if (animal === undefined) {
+    const obj = {};
+    species.forEach((specie) => {
+      obj[specie.name] = specie.residents.length;
+    });
+    return obj;
+  }
+  const filtroSpecie = species.filter((specie) => specie.name === specie.animal);
+  // const quantidadeSpecie = filtroSpecie.find((filtro) => filtro.residents.length);
+  return filtroSpecie;
 }
+
+console.log(countAnimals({ specie: 'lions' }));
 
 module.exports = countAnimals;

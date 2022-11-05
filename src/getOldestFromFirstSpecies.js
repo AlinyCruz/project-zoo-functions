7559ceb0003eb53726c2dd16b92230429e4ba3ee
Aline Oliveira Cruz <aliny.cruz@yahoo.com.br>
2022-11-05 14:53:10 -0300

    Requisitos 09

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -2,23 +2,24 @@ const { employees, species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const localizaAnimais = employees.find((employee) => employee.id === id).responsibleFor[0];
+  const localizaAnimais = employees.find((employee) => employee
+    .id === id).responsibleFor[0];
   // retorna o ID's do 1º animail do funcionário.
 
   // retorna um array de objs com os dados dos animais do funcionário.
-  const animalMaisVelho = species.find((animal) => animal
-    .id === localizaAnimais).residents.reduce((acumulador, valorCorrente) => {
-    let retornaIdade;
-    if (acumulador.age > valorCorrente.age) {
-      retornaIdade = acumulador;
+  const animalMaisVelho = species.find((animal) => animal.id === localizaAnimais).residents
+    .reduce((acumulador, valorCorrente) => {
+      let retornaIdade;
+      if (acumulador.age > valorCorrente.age) {
+        retornaIdade = acumulador;
+        return retornaIdade;
+      }
+      retornaIdade = valorCorrente;
       return retornaIdade;
-    }
-    retornaIdade = valorCorrente;
-    return retornaIdade;
-  });
+    });
   return Object.values(animalMaisVelho);
 }
 
-console.log(getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2'));
+getOldestFromFirstSpecies('b0dc644a-5335-489b-8a2c-4e086c7819a2');
 
 module.exports = getOldestFromFirstSpecies;
--- src/getSchedule.js
+++ src/getSchedule.js
@@ -30,7 +30,7 @@ const retornoObjetoDiaDaSemana = (dia) => {
   if (dia === 'Monday') {
     obj.Monday = {
       officeHour: 'CLOSED',
-      exhibition: 'The zoo will be ',
+      exhibition: 'The zoo will be closed!',
     };
   } else {
     obj[diasDaSemana(dia)] = {
@@ -45,31 +45,45 @@ const retornoObjetoDiaDaSemana = (dia) => {
 
 //----------------------------------------------------------------------------------------------
 
-const semParametro = (param) => {
+const semParametro = () => {
   const obj = {};
   const dias = Object.keys(hours);
   for (let i = 0; i < dias.length; i += 1) {
     const abertura = hours[dias[i]].open; // open -> 8
     const fechamento = hours[dias[i]].close; // close -> 6
-    if (param === 'Monday') {
-      return retornoObjetoDiaDaSemana(param);
-    }
 
     obj[diasDaSemana(dias[i])] = {
       officeHour: `Open from ${abertura}am until ${fechamento}pm`,
       exhibition: diasDoAnimal(dias[i]),
     };
-  } return obj;
+  }
+  obj[diasDaSemana(dias[6])] = {
+    officeHour: 'CLOSED',
+    exhibition: 'The zoo will be closed!',
+  };
+  return obj;
 };
 // console.log(semParametro(diasDaSemana()));
 
 //----------------------------------------------------------------------------------------------
 
+const todasEspecies = (param) => {
+  const animalSo = species.find((unico) => unico.name === param);
+  if (animalSo === undefined) {
+    return undefined;
+  }
+  return animalSo.name;
+};
+// console.log(todasEspecies());
+
+//----------------------------------------------------------------------------------------------
+
 function getSchedule(scheduleTarget) {
   // RETORNO PARA SEM PARÂMETRO OU INDEFINIDO
-  if (!scheduleTarget || !diasDaSemana(scheduleTarget)) {
+  if (!scheduleTarget || (!diasDaSemana(scheduleTarget) && !todasEspecies(scheduleTarget))) {
     return semParametro();
   }
+
   // RETORNO PARA PARÂMETRO : DIA DA SEMANA
   if (scheduleTarget === diasDaSemana(scheduleTarget)) {
     return retornoObjetoDiaDaSemana(scheduleTarget);
@@ -79,6 +93,6 @@ function getSchedule(scheduleTarget) {
     .find((animal) => animal.availability).availability;
   return retornarAnimal; // ['Wednesday','Friday','Saturday','Tuesday']
 }
-console.log(getSchedule());
+// console.log(getSchedule());
 
 module.exports = getSchedule;
